fa45e234b859ff5317dd9aa8dc76e89225e76c6e
galind <guillem.yt1@gmail.com>
2022-03-19 09:20:03 +0100

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles/main.css";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
-import Particles from "../components/particles";
 
 function App({ Component, pageProps }) {
   return (
@@ -17,7 +16,6 @@ function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/images/rocket.ico" />
       </Head>
 
-      <Particles />
       <Component {...pageProps} />
     </>
   );
